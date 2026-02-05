5bafe71690fa39b0273bb0ffce42f41c2fb2a576
Paul <you@example.com>
2026-02-05 09:07:46 +0100

    data input

--- js/index.js
+++ js/index.js
@@ -1,4 +1,27 @@
-// slå dig løs her... 
+// slå dig løs her...
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
-services.forEach(service => console.log(service.headline))
+// services.forEach((service) => console.log(service.headline));
+
+const heroDOM = document.querySelector(".hero");
+
+const imgSrc = hero.image;
+
+const imgString = /*html*/ `<img src="${imgSrc}">`;
+
+const heroContentString =
+  /*html*/
+  `
+    <figure>
+            <img src="${hero.image}" alt="">
+            <figcaption>
+                    <h2>${hero.headline}</h2>
+                    <p>${hero.copy}</p>
+                    <button><img src=${hero.icon}>Explore</button>
+            </figcaption>
+    </figure>
+
+`;
+
+heroDOM.insertAdjacentHTML("afterbegin", imgString);
+console.log(heroContentString);
